11a8ac170b21d63bc43a1f8a165dabdea73c974d
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-04-13 10:19:10 -0400

    generated class quote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote=new Quote(0,new Date(),"","","",0,0,false)
+  @Output() addQuote= new EventEmitter<Quote>();
+
+  onSubmit(form: NgForm){
+    var check=this.newQuote;
+    
+    this.addQuote.emit(this.newQuote);
+    form.resetForm(); 
+  }
 
   constructor() { }
 
